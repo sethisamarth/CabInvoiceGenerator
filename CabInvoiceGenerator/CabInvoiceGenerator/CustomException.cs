acb9171c3f27f667d8fb0208cb2f05eda8d8c02c
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-11-06 11:56:38 +0530

    Multiple Rides

---
+++ CabInvoiceGenerator/CabInvoiceGenerator/CustomException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    //custom Exception-Invalid Distance,Invalid Time
+    public class CustomException : Exception
+    {
+        public ExceptionType type;
+        public string message;
+
+        public enum ExceptionType
+        {
+            INVALID_DISTANCE, INVALID_TIME, NULL_RIDES
+        }
+        public CustomException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
